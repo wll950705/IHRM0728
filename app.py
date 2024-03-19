bfe7a8c8181c371a16f7f7dbece752f7313f1666
wll7 <10110321+wll7@user.noreply.gitee.com>
2024-03-19 18:02:45 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+# 项目基础URL -- 测试环境
+BASE_URL = "http://ihrm-test.itheima.net"
+
+# token
+TOKEN = None
+
+# 请求头数据
+headers_data = {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer bcfc9d2e-a575-4240-9569-f5f860619f97"
+}
